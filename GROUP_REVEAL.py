1e02cb1af6561c93ee9db9255ca485b2dff1b008
JerinIKannath <jerinikannath@gmail.com>
2025-04-29 15:44:23 +0530

    Update GROUP_REVEAL.py

--- GROUP_REVEAL.py
+++ GROUP_REVEAL.py
@@ -39,7 +39,7 @@ st.markdown(
 st.markdown(
     """
     <div style='text-align: center;'>
-        <h1 style='color: black;font-size: 24px;'>🔍 Discover Your Group</h1>
+        <h1 style='color: black;font-size: 24px;'>Enter Your Realm!</h1>
         <p style='color: black; font-size: 20px;'>The Supreme Leader has chosen you a path. Enter your UID to reveal your destiny!</p>
     </div>
     """,
